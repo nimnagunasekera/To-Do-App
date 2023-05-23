54c1f7036b43ad5bac48b5b2358c7b7dd45a958f
CB009100 <97610809+nimnagunasekera@users.noreply.github.com>
2023-05-23 12:43:57 +0530

    Update add_todo.dart

--- lib/pages/add_todo.dart
+++ lib/pages/add_todo.dart
@@ -37,7 +37,9 @@ class _AddTodoPageState extends State<AddTodoPage> {
                 height: 30,
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 icon: const Icon(
                   CupertinoIcons.arrow_left,
                   color: Colors.white,
